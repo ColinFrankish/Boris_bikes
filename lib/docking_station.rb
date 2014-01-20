8ea6b652fc7b2df416cb8554634d157d746ab025
Colin Frankish <colin.frankish@gmail.com>
2014-01-20 16:40:29 +0000

    Available bike filter added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -17,4 +17,7 @@ class DockingStation
   def full?
     bike_count == @capacity
   end
+  def available_bikes
+    @bikes.reject { |bike| bike.broken? }
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -27,5 +27,13 @@ let(:station) {DockingStation.new(:capacity => 20)}
   def fill_station(station)
     20.times { station.dock(Bike.new)}
   end
+  it "should provide a list of available bikes." do
+    working_bike, broken_bike = Bike.new, Bike.new
+    broken_bike.break
+    station.dock(working_bike)
+    station.dock(broken_bike)
+    expect(station.available_bikes).to eq([working_bike])
+  end
 
+  
 end
