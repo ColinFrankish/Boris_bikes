d59ab2b193266d30d1c2d870ec4121daf13300e1
Colin Frankish <colin.frankish@gmail.com>
2014-01-22 16:10:37 +0000

    added alias method - Collect broken bikes

--- lib/garage.rb
+++ lib/garage.rb
@@ -10,10 +10,23 @@ class Garage
       self.dock(bike)
     end
   end
-  def fix_the_bike(broken_bikes)
-    self.broken_bikes.each do |bike|
+
+  alias_method :old_collect_broken_bikes, :collect_broken_bikes
+
+  def collect_broken_bikes(van)
+    van.broken_bikes.each do |bike|
+    van.release(bike)
+      self.dock(bike)
       bike.fix
     end
   end
+
+
+
+  # def fix_the_bike(broken_bikes)
+  #   self.broken_bikes.each do |bike|
+  #     bike.fix
+  #   end
+  # end
 
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,29 +1,24 @@
-require './lib/van'
-require './lib/docking_station'
-require './lib/bike'
-require './lib/garage'
+require_relative '../lib/garage'
 
 describe Garage do
   let (:garage) {Garage.new}
   let (:van) {Van.new}
   let (:bike) {Bike.new}
   it "should receive a broken bike from the van" do
-      #van needs to release the bike
       bike.break
       van.dock(bike)
-      #van.release(garage)
-      garage.collect_broken_bikes(van)
-
+      garage.old_collect_broken_bikes(van)
       expect(van.broken_bikes.count).to eq(0)
-      expect(garage.broken_bikes.count).to eq(1)  #
+      expect(garage.broken_bikes.count).to eq(1)
   end
+  
+
   it "has to fix a broken bike" do
     bike.break
-    garage.dock(bike)
-    expect(garage.broken_bikes.count).to eq(1)
-    garage.fix_the_bike(garage.broken_bikes)
-
+    van.dock(bike)
+    garage.collect_broken_bikes(van)
     expect(garage.broken_bikes.count).to eq(0)
+    expect(garage.available_bikes.count).to eq(1)
   end
 
 
